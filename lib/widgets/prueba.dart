a98896ffd251058fd08604e23edbaeee460a24b6
TrejoC <trejonjamesss@gmail.com>
2021-10-08 18:55:08 -0500

    Esta es otra prueba

--- lib/widgets/prueba.dart
+++ lib/widgets/prueba.dart
@@ -5,6 +5,8 @@ class PRueba extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: const Text('Haaaaaa'),
+    );
   }
 }
