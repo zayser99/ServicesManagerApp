9ba0aa8c46b5e03116fba91c11d4a973371b8d63
ZAYSER99 <zayser99@hotmail.com>
2021-10-07 23:03:57 -0500

    pendiente setstate

--- lib/screens/home_sreen.dart
+++ lib/screens/home_sreen.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:services_manager_app/widgets/widgets.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/widgets/pendientes.dart
+++ lib/widgets/pendientes.dart
@@ -1,45 +1,59 @@
 import 'package:flutter/material.dart';
 
-class Pendientes extends StatelessWidget {
+class Pendientes extends StatefulWidget {
   const Pendientes({Key? key}) : super(key: key);
 
   @override
+  State<Pendientes> createState() => _PendientesState();
+}
+
+class _PendientesState extends State<Pendientes> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      width: double.infinity,
-      height: 200,
-      margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: const <BoxShadow>[
-          BoxShadow(
-            color: Colors.black,
-            blurRadius: 5,
-            spreadRadius: -1,
-            offset: Offset(0, 0),
-          ),
-        ],
-      ),
-      child: ListView(
-        children: [
-          listPendientes(),
-          listPendientes(),
-          listPendientes(),
-          listPendientes(),
-          listPendientes(),
-          listPendientes(),
-        ],
-      ),
-    );
+    return contenedorlista();
   }
 }
 
-Widget listPendientes() {
+Widget contenedorlista() {
+  return Container(
+    width: double.infinity,
+    height: 200,
+    margin: const EdgeInsets.only(left: 10, right: 10, top: 10),
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(10.0),
+      boxShadow: const <BoxShadow>[
+        BoxShadow(
+          color: Colors.black,
+          blurRadius: 5,
+          spreadRadius: -1,
+          offset: Offset(0, 0),
+        ),
+      ],
+    ),
+    child: ListView(
+      children: [
+        pendiente(),
+        pendiente(),
+        pendiente(),
+        pendiente(),
+        pendiente(),
+        pendiente(),
+      ],
+    ),
+  );
+}
+
+Widget pendiente() {
   bool activado = false;
   return Row(
     children: [
-      Checkbox(value: activado, onChanged: (activado) {}),
+      Checkbox(
+        value: activado,
+        onChanged: (valor) {
+          activado = valor!;
+        },
+      ),
       const Text('Trabajo de plomeria'),
     ],
   );
