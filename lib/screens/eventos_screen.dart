64340c3b41e069b0c922b9eef8ce1809f9448532
ZAYSER99 <zayser99@hotmail.com>
2021-10-20 12:05:50 -0500

    tarjeta eventos una clase separada

--- lib/screens/eventos_screen.dart
+++ lib/screens/eventos_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:services_manager_app/widgets/widgets.dart';
 
 class EventosScreen extends StatelessWidget {
   const EventosScreen({Key? key}) : super(key: key);
@@ -71,109 +72,11 @@ Widget _lisviewEvento() {
     child: SizedBox(
       width: double.infinity,
       child: ListView.builder(
-        itemCount: 15,
+        itemCount: 10,
         itemBuilder: (BuildContext context, int index) {
-          return _eventoItem();
+          return const TarjetaEventos();
         },
       ),
-    ),
-  );
-}
-
-Widget _eventoItem() {
-  return Container(
-    margin: const EdgeInsets.all(10),
-    padding: const EdgeInsets.all(10),
-    width: double.infinity,
-    height: 120,
-    decoration: BoxDecoration(
-      color: Colors.white,
-      borderRadius: BorderRadius.circular(15.0),
-      boxShadow: const <BoxShadow>[
-        BoxShadow(
-          color: Colors.black,
-          blurRadius: 5,
-          spreadRadius: -1,
-          offset: Offset(0, 0),
-        ),
-      ],
-    ),
-    child: Row(
-      children: [
-        Container(
-          padding: const EdgeInsets.only(right: 10),
-          child: Column(
-            children: const [
-              Expanded(child: Text('1'), flex: 1),
-              Expanded(
-                  child: Icon(
-                    Icons.library_books_outlined,
-                    color: Colors.indigo,
-                  ),
-                  flex: 4),
-            ],
-          ),
-        ),
-        Expanded(
-            child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              // ignore: unnecessary_const
-              children: const [
-                Expanded(
-                  child: Text(
-                    '\$500mnx',
-                    textAlign: TextAlign.left,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Text(
-                  'Evento',
-                  textAlign: TextAlign.left,
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 16,
-                      color: Colors.indigo),
-                  overflow: TextOverflow.ellipsis,
-                ),
-              ],
-            ),
-            const Text(
-              'Juan Alejandro perez lopez',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 14,
-                  color: Colors.indigo),
-              overflow: TextOverflow.ellipsis,
-            ),
-            const Text(
-              '30 de ocubre 2021 a las 12pm',
-              overflow: TextOverflow.ellipsis,
-              textAlign: TextAlign.justify,
-            ),
-            const Text(
-              'Detalles...',
-              textAlign: TextAlign.left,
-              style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 16,
-                  color: Colors.indigo),
-              overflow: TextOverflow.ellipsis,
-            ),
-          ],
-        )),
-        Container(
-          padding: const EdgeInsets.only(left: 10),
-          child: const Icon(
-            Icons.arrow_drop_down_circle_sharp,
-            color: Colors.indigo,
-          ),
-        ),
-      ],
     ),
   );
 }
